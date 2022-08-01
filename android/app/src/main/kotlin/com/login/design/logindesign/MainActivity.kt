d77e2992c826b94b771b299e91a2f7b0eaf31857
Mahmoud Magdy <mahmoudmagdy.cs@gmail.com>
2022-08-01 20:07:28 +0200

    not final

---
+++ android/app/src/main/kotlin/com/login/design/logindesign/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.login.design.logindesign
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/login_screen.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+
+var email = TextEditingController();
+var password = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                   'login',
+                style: TextStyle(
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold,
+                ),
+          ),
+              SizedBox(
+                height: 40.0,
+              ),
+              TextFormField(
+                controller: email,
+                keyboardType: TextInputType.emailAddress,
+                onFieldSubmitted: (String value)
+                {
+                  print(value);
+                },
+                decoration:InputDecoration(
+                  labelText: 'Email Address',
+                  prefixIcon: Icon(
+                    Icons.email
+                  ),
+                  border: OutlineInputBorder(),
+
+                ),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              TextFormField(
+                controller: password,
+                keyboardType: TextInputType.visiblePassword,
+                obscureText: true,
+                onFieldSubmitted: (String value)
+                {
+                  print(value);
+                },
+                decoration:InputDecoration(
+                  labelText: 'Password',
+                  prefixIcon: Icon(
+                      Icons.lock
+                  ),
+                  suffixIcon: Icon(
+                      Icons.remove_red_eye
+                  ),
+                  border: OutlineInputBorder(),
+
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                color: Colors.blue,
+                width: double.infinity,
+                child: MaterialButton(
+                    onPressed: ()
+                    {
+                      print("email: "+email.text);
+                      print("password: "+password.text);
+                    },
+
+                  child: Text(
+                    'Login',
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              )
+          ],
+    ),
+        ),
+      ),
+
+    );
+  }
+}
